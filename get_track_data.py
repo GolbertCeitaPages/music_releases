46b8bd85e3eb33240a4f7d3eee8f465a86ca61f3
GolbertCeitaPages <golbertafonso@hotmail.com>
2026-02-04 15:12:16 +0100

    added scripts to automate the track collection for dashboarding

--- get_track_data.py
+++ get_track_data.py
@@ -2,10 +2,13 @@ import requests
 import os
 import random
 import time
-from datetime import datetime
+import pandas as pd
+from datetime import datetime, timedelta
 import mysql.connector
 from dotenv import load_dotenv
 from pathlib import Path
+from datetime import datetime
+import subprocess
 
 load_dotenv(dotenv_path=Path(__file__).resolve().parent / "music_bot.env")
 
@@ -63,101 +66,108 @@ def throttle_requests(min_delay=0.1, max_delay=0.2):
     delay = random.uniform(min_delay, max_delay)
     time.sleep(delay)
 
-cursor.execute("select album_id from music_db.album_trail where track_retrieval_status = false")
+runtime_csv = pd.read_csv(Path(__file__).resolve().parent / "time_tracker" / "refresh_file_releases.csv")
+last_rundate = datetime.strptime(runtime_csv["refresh_date"].max(), "%Y-%m-%d")
 
-list_of_album_ids = cursor.fetchall()
+if datetime.today() >= last_rundate + timedelta(days=7):
 
-spot_acc_token = get_spotify_token()
+    cursor.execute("select album_id from music_db.album_trail where track_retrieval_status = false")
 
-list_for_tracks_table = []
-list_for_artist_track_translation = []
+    list_of_album_ids = cursor.fetchall()
 
-for album_id_tuple in list_of_album_ids:
-    album_id = album_id_tuple[0]
-    tracks_in_album_response = get_spotify_tracks_from_album(album_id, spot_acc_token)
-
-    if not tracks_in_album_response or "items" not in tracks_in_album_response or not tracks_in_album_response["items"]:
-        # log warning
-        continue
-
-    list_of_track_ids = [track['id'] for track in tracks_in_album_response["items"]]
-    track_response = get_spotify_tracks(list_of_track_ids, spot_acc_token)
-
-    if not track_response or "tracks" not in track_response or not track_response["tracks"]:
-        # log warning
-        continue
+    spot_acc_token = get_spotify_token()
 
     list_for_tracks_table = []
     list_for_artist_track_translation = []
 
-    for track in track_response['tracks']:
-        minutes, seconds = divmod(int(track['duration_ms'] / 1000), 60)
+    for album_id_tuple in list_of_album_ids:
+        album_id = album_id_tuple[0]
+        tracks_in_album_response = get_spotify_tracks_from_album(album_id, spot_acc_token)
 
-        list_for_tracks_table.append((
-            track['name'],
-            ', '.join([a['name'] for a in track['artists']]),
-            ', '.join([i['id'] for i in track['artists']]),
-            album_id,  # album id
-            track['disc_number'],
-            track['track_number'],
-            track['type'],
-            track['popularity'],
-            track['duration_ms'],
-            round(track['duration_ms'] / 1000, 2),
-            f"{minutes}:{seconds:02d}",
-            track['explicit'],
-            track['is_local'],
-            ', '.join(track['available_markets']),
-            track['id'],
-            track['external_urls']['spotify'],
-            track['href'],
-            track['uri'],
-            track['preview_url'],
-            datetime.now()
-        ))
-        for tl_artist in track["artists"]:
-            list_for_artist_track_translation.append((
-                track["id"],
-                tl_artist["id"],
+        if not tracks_in_album_response or "items" not in tracks_in_album_response or not tracks_in_album_response["items"]:
+            # log warning
+            continue
+
+        list_of_track_ids = [track['id'] for track in tracks_in_album_response["items"]]
+        track_response = get_spotify_tracks(list_of_track_ids, spot_acc_token)
+
+        if not track_response or "tracks" not in track_response or not track_response["tracks"]:
+            # log warning
+            continue
+
+        list_for_tracks_table = []
+        list_for_artist_track_translation = []
+
+        for track in track_response['tracks']:
+            minutes, seconds = divmod(int(track['duration_ms'] / 1000), 60)
+
+            list_for_tracks_table.append((
+                track['name'],
+                ', '.join([a['name'] for a in track['artists']]),
+                ', '.join([i['id'] for i in track['artists']]),
+                album_id,  # album id
+                track['disc_number'],
+                track['track_number'],
+                track['type'],
+                track['popularity'],
+                track['duration_ms'],
+                round(track['duration_ms'] / 1000, 2),
+                f"{minutes}:{seconds:02d}",
+                track['explicit'],
+                track['is_local'],
+                ', '.join(track['available_markets']),
+                track['id'],
+                track['external_urls']['spotify'],
+                track['href'],
+                track['uri'],
+                track['preview_url'],
                 datetime.now()
             ))
+            for tl_artist in track["artists"]:
+                list_for_artist_track_translation.append((
+                    track["id"],
+                    tl_artist["id"],
+                    datetime.now()
+                ))
 
-    # Insert per album
-    cursor.executemany("""
-        insert into music_db.track_data (track_name, artists, artist_ids, album_id, disc_number,
-        track_number, `type`, popularity, duration_ms, duration_s, duration_m_s, explicit, is_local,
-        available_markets, track_id, spotify_url, href, uri, preview_url, `timestamp`)
-        values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """, list_for_tracks_table)
+        # Insert per album
+        cursor.executemany("""
+            insert into music_db.track_data (track_name, artists, artist_ids, album_id, disc_number,
+            track_number, `type`, popularity, duration_ms, duration_s, duration_m_s, explicit, is_local,
+            available_markets, track_id, spotify_url, href, uri, preview_url, `timestamp`)
+            values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, list_for_tracks_table)
 
-    cursor.executemany("""
-        insert into music_db.artist_track_translation_table (track_id, artist_id, `timestamp`)
-        values (%s, %s, %s)
-    """, list_for_artist_track_translation)
+        cursor.executemany("""
+            insert into music_db.artist_track_translation_table (track_id, artist_id, `timestamp`)
+            values (%s, %s, %s)
+        """, list_for_artist_track_translation)
 
-    # update album status if inserts succeeded
-    cursor.execute("""
-        update music_db.album_trail
-        SET track_retrieval_status = TRUE
-        WHERE album_id = %s
-    """, (album_id,))
+        # update album status if inserts succeeded
+        cursor.execute("""
+            update music_db.album_trail
+            SET track_retrieval_status = TRUE
+            WHERE album_id = %s
+        """, (album_id,))
 
-    connection.commit()
+        connection.commit()
 
-    throttle_requests()
+        throttle_requests()
 
 
-album_ids_to_update = [ids[0] for ids in list_of_album_ids]
+    album_ids_to_update = [ids[0] for ids in list_of_album_ids]
 
-if album_ids_to_update:  # only run if not empty
-    update_query = """
-        update music_db.album_trail
-        set track_retrieval_status = true
-        where album_id in ({})
-    """.format(", ".join(["%s"] * len(album_ids_to_update)))
+    if album_ids_to_update:  # only run if not empty
+        update_query = """
+            update music_db.album_trail
+            set track_retrieval_status = true
+            where album_id in ({})
+        """.format(", ".join(["%s"] * len(album_ids_to_update)))
 
-    cursor.execute(update_query, album_ids_to_update)
-    connection.commit()
+        cursor.execute(update_query, album_ids_to_update)
+        connection.commit()
 
-cursor.close()
-connection.close()
+    cursor.close()
+    connection.close()
+
+    subprocess.run(["python", str(Path(__file__).resolve().parent / "refresh_script_tracks.py")])
---
+++ refresh_script_tracks.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+
+def incremental_load():
+    refresh_file = pd.read_csv(Path(__file__).resolve().parent / "time_tracker" / "refresh_file_tracks.csv")
+
+    timestamp = datetime.now()
+    today = datetime.today().date()
+
+    new_row = pd.DataFrame([[today,timestamp]], columns=['refresh_date', 'timestamp'])
+
+    refresh_file = pd.concat([refresh_file, new_row], ignore_index=True)
+
+    refresh_file.to_csv(Path(__file__).resolve().parent / "time_tracker" / "refresh_file_tracks.csv", index=False)
+
+def full_refresh():
+    timestamp = datetime.now()
+    today = datetime.today().date()
+
+    refresh_file = pd.DataFrame([[today,timestamp]], columns=['refresh_date', 'timestamp'])
+
+    refresh_file.to_csv(Path(__file__).resolve().parent / "time_tracker" / "refresh_file_tracks.csv", index=False)
+
+incremental_load()
